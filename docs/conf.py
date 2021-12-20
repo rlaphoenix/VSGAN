0a1a9f2e2b03d1c969992bd17207711a5893a201
rlaphoenix <pragma.exe@gmail.com>
2021-12-20 02:47:29 +0000

    docs: Get sidebar image path from conf

--- docs/conf.py
+++ docs/conf.py
@@ -50,6 +50,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #
 html_theme = 'furo'
 
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+html_logo = '_static/images/icon.png'
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
