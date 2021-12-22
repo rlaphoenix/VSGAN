a665ccd6db6a33f3cf2a06f8949e598d8610b537
rlaphoenix <pragma.exe@gmail.com>
2021-12-22 00:03:15 +0000

    Add support for RealESRGAN models

--- vsgan/archs/__init__.py
+++ vsgan/archs/__init__.py
@@ -3,8 +3,9 @@ from typing import Union, OrderedDict
 from torch import Tensor
 
 from vsgan.archs.ESRGAN import ESRGAN
+from vsgan.archs.RealESRGAN import RealESRGAN
 
-MODEL_T = ESRGAN
+MODEL_T = Union[ESRGAN, RealESRGAN]
 STATE_T = OrderedDict[str, Tensor]
 
-__ALL__ = (MODEL_T, STATE_T, ESRGAN)
+__ALL__ = (MODEL_T, STATE_T, ESRGAN, RealESRGAN)
