b942a04b9bfa0bd6c9a7d7ed315376754a910a76
rlaphoenix <pragma.exe@gmail.com>
2021-12-22 03:46:14 +0000

    Delete RealESRGAN.py, use normal ESRGAN instead
    The normal ESRGAN class now supports Real-ESRGAN model files, and does a better job at it with better auto-detection on scales, number of feats, blocks, and so on.

--- vsgan/archs/__init__.py
+++ vsgan/archs/__init__.py
@@ -1,9 +1,5 @@
-from typing import Union
-
-from vsgan.archs import ESRGAN, RealESRGAN
 from vsgan.archs.ESRGAN import ESRGAN
-from vsgan.archs.RealESRGAN import RealESRGAN
 
-MODEL_T = Union[ESRGAN, RealESRGAN]
+MODEL_T = ESRGAN
 
-__ALL__ = (MODEL_T, ESRGAN, RealESRGAN)
+__ALL__ = (MODEL_T, ESRGAN)
