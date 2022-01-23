2f94f0e9a753b02af23dada5eada16612451eada
rlaphoenix <pragma.exe@gmail.com>
2022-01-23 10:44:07 +0000

    Add VS_API and IS_VS_API_4 constants

--- vsgan/constants.py
+++ vsgan/constants.py
@@ -3,7 +3,11 @@ from __future__ import annotations
 from typing import OrderedDict
 
 import numpy as np
+import vapoursynth as vs
 from torch import Tensor
+
+VS_API = vs.__api_version__.api_major
+IS_VS_API_4 = VS_API == 4
 
 MAX_DTYPE_VALUES = {
     np.dtype("int8"): 127,
