4cda4ed23a602f95c7b452f68bcb68d6ea5d1216
rlaphoenix <pragma.exe@gmail.com>
2022-01-25 20:10:07 +0000

    Whoops, did not mean to commit that :poop:

--- vsgan/utilities.py
+++ vsgan/utilities.py
@@ -139,8 +139,6 @@ def recursive_tile_tensor(
         gc.collect()
         raise RecursionError(f"Exceeded maximum tiling recursion of 10...")
 
-    input("Waiting")
-
     if max_depth is None or max_depth == current_depth:
         # attempt non-tiled super-resolution if no known depth, or at depth
         try:
